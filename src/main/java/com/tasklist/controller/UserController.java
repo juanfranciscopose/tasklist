780201ec3910507f0a88732b5691f3af8355f652
jfp1989 <juanfranciscopose@gmail.com>
2020-08-04 19:00:16 -0300

    requestmaping en usercontroller

--- src/main/java/com/tasklist/controller/UserController.java
+++ src/main/java/com/tasklist/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tasklist.dto.TaskRequest;
@@ -24,6 +25,7 @@ import com.tasklist.util.exception.UnprocessableEntityException;
 import com.tasklist.util.validator.UserValidator;
 
 @RestController
+@RequestMapping
 @CrossOrigin
 public class UserController {
 	
